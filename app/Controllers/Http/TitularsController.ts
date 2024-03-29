867692dffc902191b8aef49443feecc129e1a3f6
Molton321 <molton334@gmail.com>
2024-03-29 14:09:07 -0500

    Titulars-first commit

---
+++ app/Controllers/Http/TitularsController.ts
@@ -0,0 +1,39 @@
+import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import Titular from 'App/Models/Titular'
+
+export default class TitularsController {
+  public async find({ request, params }: HttpContextContract) {
+    if (params.id) {
+      return Titular.findOrFail(params.id)
+    } else {
+      const data = request.all()
+      if ('page' in data && 'per_page' in data) {
+        const page = request.input('page', 1)
+        const perPage = request.input('per_page', 20)
+        return await Titular.query().paginate(page, perPage)
+      } else {
+        return await Titular.query()
+      }
+    }
+  }
+  public async create({ request }: HttpContextContract) {
+    const body = request.body()
+    const theTitular: Titular = await Titular.create(body)
+    return theTitular
+  }
+
+  public async update({ params, request }: HttpContextContract) {
+    const theTitular: Titular = await Titular.findOrFail(params.id)
+    const body = request.body()
+    theTitular.name = body.name
+    theTitular.email = body.email
+    theTitular.phone = body.phone
+    return theTitular.save()
+  }
+
+  public async delete({ params, response }: HttpContextContract) {
+    const theTitular: Titular = await Titular.findOrFail(params.id)
+    response.status(204)
+    return theTitular.delete()
+  }
+}
