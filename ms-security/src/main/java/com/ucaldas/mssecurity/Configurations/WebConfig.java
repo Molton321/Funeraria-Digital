50bb416df113d020a67767a3f1e116918e9aaf62
Carlos Alape <145057710+CarlosAlape39671@users.noreply.github.com>
2024-04-15 20:53:05 -0500

    cambios en WebConfig

--- ms-security/src/main/java/com/ucaldas/mssecurity/Configurations/WebConfig.java
+++ ms-security/src/main/java/com/ucaldas/mssecurity/Configurations/WebConfig.java
@@ -13,10 +13,8 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
-        /*
         registry.addInterceptor(securityInterceptor)
                 .addPathPatterns("/api/**")
                 .excludePathPatterns("/api/public/**");
-         */
     }
 }
