f0d373aa5b3ba81bfb1418e3fd1317d1f93ccaae
Carlos Alape <145057710+CarlosAlape39671@users.noreply.github.com>
2024-05-09 12:19:08 -0500

    Relación de titular.

--- Ms-Negocio/app/Models/Titular.ts
+++ Ms-Negocio/app/Models/Titular.ts
@@ -1,5 +1,6 @@
 import { DateTime } from 'luxon'
-import { BaseModel, column } from '@ioc:Adonis/Lucid/Orm'
+import { BaseModel, BelongsTo, belongsTo, column } from '@ioc:Adonis/Lucid/Orm'
+import Client from './Client'
 
 export default class Titular extends BaseModel {
   @column({ isPrimary: true })
@@ -14,5 +15,8 @@ export default class Titular extends BaseModel {
   @column.dateTime({ autoCreate: true, autoUpdate: true })
   public updatedAt: DateTime
 
-  //TODO: Add the relationship with the client and beneficiary
+  @belongsTo(() => Client, {
+    foreignKey: 'client_id'
+  })
+  public client: BelongsTo<typeof Client>
 }
