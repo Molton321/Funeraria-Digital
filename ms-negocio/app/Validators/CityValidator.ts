020562a4da96ff5ff7e0a74c5b616f86c2fb194c
Molton321 <molton334@gmail.com>
2024-05-10 01:49:59 -0500

    Validators mod

--- ms-negocio/app/Validators/CityValidator.ts
+++ ms-negocio/app/Validators/CityValidator.ts
@@ -1,13 +1,5 @@
 import { schema, CustomMessages,rules } from '@ioc:Adonis/Core/Validator'
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
-import Department from 'App/Models/Department'
-import { column } from '@ioc:Adonis/Lucid/Orm'
-
-const positiveNumberRule = (value: number) => {
-  if (value < 0) {
-    return 'El número debe ser mayor o igual a cero';
-  }
-}
 
 export default class CityValidator {
   constructor(protected ctx: HttpContextContract) {}
@@ -16,7 +8,7 @@ export default class CityValidator {
   public schema = schema.create({
     id : schema.number([rules.exists({table: 'cities', column: 'id'}), rules.range(0,100000)]),  
     city_name : schema.string([rules.minLength(4)]),
-    department_id : schema.number([rules.exists({table: 'department', column: 'id'}), rules.range(0,100000)])
+    department_id : schema.number([rules.exists({table: 'department', column: 'id'})])
   })
 
   public messages: CustomMessages = {}
