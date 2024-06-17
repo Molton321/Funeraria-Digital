2a0f33d38cbaa6f0bfdd7d54348feab933fb40d3
Molton321 <molton334@gmail.com>
2024-06-17 14:38:13 -0500

    Cambios de login

--- frontend/src/app/pages/user-profile/user-profile.component.ts
+++ frontend/src/app/pages/user-profile/user-profile.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { User } from 'src/app/models/user/user.model';
+import { SecurityService } from 'src/app/services/security/security.service';
 
 @Component({
   selector: 'app-user-profile',
@@ -6,10 +9,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user-profile.component.scss']
 })
 export class UserProfileComponent implements OnInit {
+  theUser: User;
+  subscription: Subscription;
 
-  constructor() { }
+  constructor(private securityService: SecurityService) { }
 
   ngOnInit() {
+    this.subscription = this.securityService.getUser().subscribe(user => {
+      this.theUser = user;
+    })
   }
 
 }
